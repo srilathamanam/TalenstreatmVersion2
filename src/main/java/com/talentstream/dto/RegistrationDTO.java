eb5cb2cefdd2de81ed804e9c334b3429c942bbe0
srilathamanam <srilatha.manam@gmail.com>
2023-12-08 16:12:36 +0530

    Update RegistrationDTO.java

--- src/main/java/com/talentstream/dto/RegistrationDTO.java
+++ src/main/java/com/talentstream/dto/RegistrationDTO.java
@@ -2,10 +2,38 @@ package com.talentstream.dto;
 
 import lombok.Data;
 
-@Data
+
 public class RegistrationDTO {
 	 private String name;
 	    private String email;
 	    private String mobileNumber;
 	    private String password;
+	
+public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		
+		public String getMobilenumber() {
+			return mobilenumber;
+		}
+		public void setMobilenumber(String mobilenumber) {
+			this.mobilenumber = mobilenumber;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public void setPassword(String password) {
+			this.password = password;
+		}
+	    
 }
+
