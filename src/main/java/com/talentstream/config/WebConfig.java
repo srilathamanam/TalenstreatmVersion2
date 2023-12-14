1a4c8d5be60110e300f0b14a634645244a6536fa
srilathamanam <srilatha.manam@gmail.com>
2023-12-14 15:44:42 +0530

    Update WebConfig.java

--- src/main/java/com/talentstream/config/WebConfig.java
+++ src/main/java/com/talentstream/config/WebConfig.java
@@ -27,9 +27,9 @@ public class WebConfig {
    //https://talent-tracker-front-k336pfj8a-madar1992.vercel.app
         
      // config.addAllowedOrigin("http://localhost:3000");
-        config.addAllowedOrigin("https://talent-stream-front-end-version2-eedekarunakar.vercel.app");
+      //  config.addAllowedOrigin("https://talent-stream-front-end-version2-eedekarunakar.vercel.app");
         
-
+config.addAllowedOrigin("talent-stream-front-end-version2-djgullodv-eedekarunakar.vercel.app");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
