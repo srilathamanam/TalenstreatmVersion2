7b78a68b551ecde00b896cde90acfe649c545fcf
srilathamanam <srilatha.manam@gmail.com>
2023-12-14 16:03:15 +0530

    Update WebConfig.java

--- src/main/java/com/talentstream/config/WebConfig.java
+++ src/main/java/com/talentstream/config/WebConfig.java
@@ -9,27 +9,29 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+ 
 import java.util.Arrays;
-
+ 
 @Configuration
 //@EnableWebMvc
 public class WebConfig {
-
+ 
     private static final Long MAX_AGE = 3600L;
     private static final int CORS_FILTER_ORDER = -102;
-
+ 
+    public void configure(WebSecurity webSecurity) {
+        webSecurity.ignoring().antMatchers("/public/images/**");
+    }
     @Bean
     public FilterRegistrationBean corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-       // config.setAllowCredentials(true);
-   //https://talent-tracker-front-k336pfj8a-madar1992.vercel.app
-        
-     config.addAllowedOrigin("http://localhost:3000");
-        config.addAllowedOrigin("https://talent-stream-front-end-version2-eedekarunakar.vercel.app");
-        
-//config.addAllowedOrigin("talent-stream-front-end-version2-djgullodv-eedekarunakar.vercel.app");
+        //config.setAllowCredentials(true);
+
+        //config.addAllowedOrigin("http://localhost:3000");
+        //  config.addAllowedOrigin("https://talent-stream-front-end-version2-kwrsb2zt1-eedekarunakar.vercel.app");
+            config.addAllowedOrigin("https://talent-stream-front-end-version2-eedekarunakar.vercel.app");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
@@ -42,13 +44,14 @@ public class WebConfig {
         config.setMaxAge(MAX_AGE);
         source.registerCorsConfiguration("/**", config);
         FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-
+ 
         // should be set order to -100 because we need to CorsFilter before SpringSecurityFilter
         bean.setOrder(CORS_FILTER_ORDER);
         return bean;
     }
-    public void configure(WebSecurity webSecurity) {
+public void configure(WebSecurity webSecurity) {
         webSecurity.ignoring().antMatchers("/applicant/photos/**");
         webSecurity.ignoring().antMatchers("/applicant/resumes/**");
     }
 }
+ 
