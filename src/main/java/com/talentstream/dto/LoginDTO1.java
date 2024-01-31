2feb356e3d21ebd519ee83f33fe968161ac5607b
srilathamanam <srilatha.manam@gmail.com>
2024-01-31 17:31:18 +0530

    Add files via upload

---
+++ src/main/java/com/talentstream/dto/LoginDTO1.java
@@ -0,0 +1,15 @@
+package com.talentstream.dto;
+
+public class LoginDTO1 {
+
+	private String email;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
