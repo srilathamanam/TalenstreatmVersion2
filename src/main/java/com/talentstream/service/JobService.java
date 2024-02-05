f5fd91363f5c4038107565a784d3598fd70392ca
srilathamanam <srilatha.manam@gmail.com>
2024-02-05 18:33:08 +0530

    Update JobService.java

--- src/main/java/com/talentstream/service/JobService.java
+++ src/main/java/com/talentstream/service/JobService.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import java.util.HashSet;
 import com.talentstream.dto.JobDTO;
 import com.talentstream.dto.RecuriterSkillsDTO;
 import com.talentstream.entity.CompanyProfile;
