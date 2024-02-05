60a010d119ecf691372d54a056013a8f8d617a7c
srilathamanam <srilatha.manam@gmail.com>
2024-02-05 18:24:43 +0530

    Update JobController.java

--- src/main/java/com/talentstream/controller/JobController.java
+++ src/main/java/com/talentstream/controller/JobController.java
@@ -12,6 +12,7 @@ import com.talentstream.entity.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
