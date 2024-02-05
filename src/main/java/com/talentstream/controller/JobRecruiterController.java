c1888c4cb60eae59b738e0d0cfc54b2fa453a649
srilathamanam <srilatha.manam@gmail.com>
2024-02-05 19:48:56 +0530

    Update JobRecruiterController.java

--- src/main/java/com/talentstream/controller/JobRecruiterController.java
+++ src/main/java/com/talentstream/controller/JobRecruiterController.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
