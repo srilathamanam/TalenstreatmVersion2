2d7f5fb828189a1c0c621ed86b8aa6dc4bdad3a9
srilathamanam <srilatha.manam@gmail.com>
2024-02-21 19:26:25 +0530

    Update ApplyJobController.java

--- src/main/java/com/talentstream/controller/ApplyJobController.java
+++ src/main/java/com/talentstream/controller/ApplyJobController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.talentstream.entity.AppliedApplicantInfo;
 import com.talentstream.dto.JobDTO;
 import com.talentstream.dto.ScheduleInterviewDTO;
 import com.talentstream.entity.Alerts;
